ae248d02ce94b2614b5de9ea4c36bd97f363a0a8
Abdul Wahed Nur <wahednur@gmail.com>
2025-08-21 22:28:31 +0600

    Login page added

---
+++ src/pages/auth/social-login.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function SocialLogin() {
+  return (
+    <Button variant="outline" className="w-full">
+      Login with Google
+    </Button>
+  );
+}
